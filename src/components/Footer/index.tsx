2f762f1fba06e26f12e3f6e239b4322cda6f56ad
DeyvisonSLS <deyvisonsls@proton.me>
2026-07-14 15:39:12 -0300

    fix(ui): change footer container from div to footer element for semantic markup

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -2,12 +2,13 @@ import styles from './styles.module.css';
 
 export function Footer() {
   return (
-    <div className={styles.footerContainer}>
+    <footer className={styles.footerContainer}>
       <p>Understanding the pomodoro technique 🍅</p>
+      <p>React Pomodoro &copy; {new Date().getFullYear()}</p>
       <p>
         React Pomodoro - Made with 💘 by{' '}
         <a href='https://deyvison.com.br'>Deyvison</a>
       </p>
-    </div>
+    </footer>
   );
 }
